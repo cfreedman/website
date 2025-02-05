98030197b332783fa8c95a7d275a85bb39f4f4c8
Collum <collum.freedman@gmail.com>
2025-02-04 23:25:38 -0500

    Add to raytracer blog post and work of styling for code snippets

--- src/app/posts/raytracer/page.tsx
+++ src/app/posts/raytracer/page.tsx
@@ -21,9 +21,159 @@ export default function Page(): JSX.Element {
       </p>
       <h4>The Inner Workings of a Raytracer</h4>
       <h4>Using the Right Polymorphism in Rust</h4>
+      <p>
+        Implementing some of the fundamental elements in the raytracer offered
+        an interesting design decision regarding how to best represent abstract
+        sets of type with unifying properties or behavior. Important in this
+        setting are <b>geometries</b>, defining the physical boundaries and
+        structure of objects in world space, <b>materials</b>, defining how
+        light rays interact with objects when they are hit, and <b>textures</b>,
+        which assign color values to objects along their surfaces.
+      </p>
+      <p>
+        Geometry must be <i>hittable</i> - rays traversing the scene must be
+        able to determine whether or no they will collide with it. Materials
+        must be <i>scatterable</i> - upon collision, there must be some way to
+        determine how ray will (or will not) scatter off the surface to continue
+        traversing the scene. And textures should determine a <i>value</i> or
+        color to output at a specific point a ray hits on the surface.
+      </p>
+      <p>
+        The commonalities across the behavior of, for example, a sphere and a
+        quad, both geometries and both needing some implementation of this{" "}
+        <i>hittable</i> behavior, suggests the need for some abstraction over
+        these different subtypes. Rust offers its own take on{" "}
+        <b>polymorphism</b> with several differences from other more common
+        approaches in object oriented programming. In particular, there is no
+        concept of inheritance or subclassing. Instead, Rust uses <b>enums</b>{" "}
+        to define abstractions over a closed set of types, <b>generics</b> to
+        abstract to different possible types, and <b>traits</b> (the Rust
+        version of interfaces) to impose constraints on what some types must
+        provide in terms of data and behavior.
+      </p>
+      <p>
+        In its current state, I&apos;m not happy with how I implemented
+        materials. I used an enum to define an overarching <code>Material</code>{" "}
+        type to wrap the four variants like so
+      </p>
+      <pre>
+        <code>
+          {`
+        pub enum Material {
+            Lambertian(Lambertian),
+            Dielectric(Dielectric),
+            Metal(Metal),
+            Isotropic(Isotropic),
+        }
+      `}
+        </code>
+      </pre>
+      <p>
+        The four variant types <code>Lambertian</code>, <code>Dielectric</code>,{" "}
+        <code>Metal</code>, and <code>Isotropic</code>, then each implemented
+        their own individual version of a scatter function, which are used in
+        the Material in a match statement to call the correct scatter depending
+        on the underlying variant.
+      </p>
+      <pre>
+        <code>{`
+              impl Material {
+                  pub fn scatter(
+                      &self,
+                      ray_in: Ray,
+                      hit_data: &mut HitData,
+                      attenuation: &mut Vec3,
+                      scattered: &mut Ray,
+                  ) -> bool {
+                      match self {
+                          Self::Lambertian(lamb) => lamb.scatter(ray_in, hit_data, attenuation, scattered),
+                          Self::Metal(metal) => metal.scatter(ray_in, hit_data, attenuation, scattered),
+                          Self::Dielectric(dielectric) => {
+                              dielectric.scatter(ray_in, hit_data, attenuation, scattered)
+                          }
+                          Self::DiffuseLight(_) => false,
+                          Self::Isotropic(isotropic) => {
+                              isotropic.scatter(ray_in, hit_data, attenuation, scattered)
+                          }
+                      }
+                  }
+                
+              }
+              
+              ...
+              
+              impl Lambertian {
+                  pub fn scatter( ... ) -> bool { ... }
+              }
+
+              impl Dielectric {
+                  pub fn scatter( ... ) -> bool { ... }
+              }
+
+              impl Metal {
+                  pub fn scatter( ... ) -> bool { ... }
+              }
+
+              impl Isotropic {
+                  pub fn scatter( ... ) -> bool { ... }
+              }
+                        
+          `}</code>
+      </pre>
+      <p>
+        A better solution, I think, would be to define <code>Material</code> as
+        a trait with a shared scatter function that participating types must
+        implement.
+      </p>
+      <pre>
+        <code>
+          {`
+          pub trait Material {
+              pub fn scatter(
+                  &self,
+                  ray_in: Ray,
+                  hit_data: &mut HitData,
+                  attenuation: &mut Vec3,
+                  scattered: &mut Ray,
+              ) -> bool {}
+          }
+        `}
+        </code>
+      </pre>
+      <p>
+        In looking back, this captures better the abstraction of materials being
+        an arbitrary, open number of types bound together by a closed set of
+        behavior - exactly how rays should scatter off of them, rather than a
+        closed set of types with arbitrary open properties across them. The
+        former is a trait (or interface) over types, and the latter is an enum,
+        binding types together under one parent representative all variants.
+      </p>
+      <p>
+        In part, some of what drew me first to the enum representation is that,
+        provided all the variant implement the Rust <code>Copy</code> trait, it
+        can automatically be derived on the enum parent as well. As soon as a
+        type implements <code>Copy</code>, this eliminates much of the
+        difficulty Rust is famous for involving its memory management rules and
+        ownership. Types with <code>Copy</code> can be thought of as purely
+        stack-allocated and, in passing them into functions as arguments or
+        other execution blocks, it makes no meaningful difference to the
+        compiler if you use the value itself or a reference to it. Instead, that
+        type is immediately <i>passed-by-value</i> into function calls by the
+        Rust compiler.
+      </p>
+      <p>
+        In contast, when refererring to a trait object in the polymorphic way
+        necessary for our raytracer, Rust requires us to use a{" "}
+        <code>{`Box<dyn Material>`}</code>, which is heap allocatted. In this
+        case, all of the particularities of moved ownership and reference
+        management come into play, requiring much more careful attention.
+        Ultimately though, this is the better abstraction and there no need to
+        shy away from the features that, though lending some additional
+        difficulty, give Rust much of its safety and power.
+      </p>
       <h4>Bug of the Project (BOTP)!</h4>
       <p className="mb-[18px]">
-        Debugging raytracing seems to me to be a uniquely entertaining and
+        Debugging raytracing seems to me to be a particularly entertaining and
         challenging undertaking. You are often left deciphering a wildly
         unexpected or inaccurate end image and left without much clue as to what
         bug in the mass of accumulating math calculations led to this result.
@@ -43,19 +193,78 @@ export default function Page(): JSX.Element {
         bouncing between two surfaces repeatedly before eventually escaping to
         terminate in a skybox hit or hitting the upper recursion limit set for
         the program. Each bounce and thus further recursion into the raytracing
-        routine adds a darkening factor to the eventual output color for that
-        particular ray calculation.
+        routine adds an attenuating darkening factor to the eventual output
+        color for that particular ray calculation. That calculation original
+        from this code snippet below:
+      </p>
+      <pre>
+        <code>{`
+          if let Some(material) = hit_data.clone().material {
+              let emitted_color = material.emit(hit_data.point, hit_data.u, hit_data.v);
+              if !material.scatter(ray, &mut hit_data, &mut attenuation, &mut scattered) {
+                  return emitted_color;
+              }
+
+              return emitted_color + attenuation * self.ray_color(scattered, depth - 1, world);
+          }
+        `}</code>
+      </pre>
+      <p>
+        Any ray making contact with a material will scatter with the material
+        imparting an <code>attenuation</code> factor to the later recursive call
+        that is by definition in (0,1) in the normalized color space, leading to
+        darkening of the resultant color. It makes sense then that two surfaces
+        close together will produce clear shadowing since rays scattered on
+        average like the the surface&apos;s normal and are highly likely to
+        collide with the second surface just above and vice versa, leading to
+        this exact recursive darkening cycle. However, this effect should
+        decrease substantially with additional distance between the two
+        surfaces, since more scattering rays will avoid collision between the
+        pair surfaces and escape into other parts of the scene, avoiding the
+        cycle. In my scene, the shadowing instead appears too strong relative to
+        the distance between the planes.
       </p>
       <p>
-        It makes sense then that two surfaces close together will produce clear
-        shadowing since rays scattered on average like the the surface&apos;s
-        normal and are highly likely to collide with the second surface just
-        above and vice versa, leading to this exact recursive darkening cycle.
-        However, this effect should decrease substantially with additional
-        distance between the two surfaces, since more scattering rays will avoid
-        collision between the pair surfaces and escape into other parts of the
-        scene, avoiding the cycle. In my scene, the shadowing appears to strong
-        relative to the distance between the planes.
+        When I first came across this issue, my first steps were to try to
+        reproduce it under a variety of different conditions. With two different
+        types of geometry primitives (spheres and quads), four different
+        materials (Lambertian, Dielectric, Metals, and Isotropics), and
+        different texturing options, it made sense to narrow down whether this
+        only occurred in a subset of these elements. It also helped that I could
+        scale down high-fidelity inputs like the recursive call limit and the
+        final image resolution to improve runtime while checking for the
+        presence of these inaccurate shadows.
+      </p>
+      <p>
+        In the end, it seemed that only <b>quads</b> with <b>Lambertian</b>{" "}
+        materials would cast the shadow artifacts. Although this narrowed down
+        the source of the issue, the be completely sure, I set up a dummy scene
+        with two quads directly in line along the vertical axis so that I would
+        presumably get full coverage of the shadow of one atop the other.
+        Calculating and logging the variation of the angle of the scattered ray
+        from the plane normal revealed the problem: the scattered rays off of
+        the Lambertian quads were using a non-normalized normal!
+      </p>
+      <p>
+        Lambertian reflectance commonly uses a simple trick of summing together
+        a surfaces normal with a randomly sampled unit vector to generate the
+        distribution of scattered vector coming off the surface and the correct
+        visual diffuse pattern.
+      </p>
+      <pre>
+        <code>{`
+          let mut scatter_direction = hit_data.normal + Vec3::random_unit_vector();
+        `}</code>
+      </pre>
+      <p>
+        Importantly, the normal vector should be normalized to a unit vector,
+        otherwise the magnitude of the two vectors involved in the summation
+        will not match and the normal could possibly completely dominate the
+        resultant scattering direction, leading to a distribution much too
+        tightly centered around the normal direction. This is exactly what
+        occurred for me and produced the extreme shadows through repeated,
+        incorrect normal scattering between planes. A simple corrected to a unit
+        vector normal fixed everything!
       </p>
       <p className="mb-[18px]"></p>
       <h4>Further Refinements and Next Steps</h4>
