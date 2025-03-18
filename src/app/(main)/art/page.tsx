f32b9f6348a0fbd1e212b145cbe17896e9ddff9d
Collum <collum.freedman@gmail.com>
2025-03-17 23:58:07 -0400

    quick edit

--- src/app/(main)/art/page.tsx
+++ src/app/(main)/art/page.tsx
@@ -4,7 +4,7 @@ export default function ArtPage(): JSX.Element {
   return (
     <>
       <h3>
-        Plans to add some Blender art here once I&apos;m happy enough with it.
+        Todo: add some Blender art here once I&apos;m happy enough with it.
       </h3>
     </>
   );
